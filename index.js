3a7f81bb22d5e1f1464495f666d900c38cd688d1
Zachary Soliman <zachsoliman1@gmail.com>
2022-07-04 15:35:48 -0400

    all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook,  + ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+const shortGreeting = 'Welcome, ' + 'GH' + '!';
